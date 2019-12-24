69e0914a1f7d93560c966f9bdb1dcb702d332823
Fabricio <fabriciogiac@gmail.com>
2019-12-24 06:42:44 -0300

    update flag description

--- config.go
+++ config.go
@@ -14,7 +14,7 @@ type Config struct {
 
 // ReadConfig reads the arguments from the command line
 func ReadConfig() Config {
-	targetURL := flag.String("url", "https://jsonplaceholder.typicode.com", "Required. Set the base url you want to capture")
+	targetURL := flag.String("url", "https://jsonplaceholder.typicode.com", "Required. Set the url you want to proxy")
 	proxyPort := flag.String("port", "9000", "Set the proxy port")
 	dashboardPort := flag.String("dashboard", "9001", "Set the dashboard port")
 	maxCaptures := flag.Int("captures", 16, "Set how many captures to show in the dashboard")
